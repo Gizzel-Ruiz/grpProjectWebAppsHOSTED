d9cc8e1fe96cfb2bacf906ce50293a1d34c58237
Gizzel-Ruiz <gruiz015@jaguar.tamu.edu>
2022-07-29 15:30:30 -0500

    db test

---
+++ mongodb.js
@@ -0,0 +1,3 @@
+const mongo = 'mongodb+srv://gruiz8:SSkzYMX3sHak4HHj@cluster0.klvn1.mongodb.net/WebAppProjectDB'
+
+module.exports = mongo;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mongo = require('./mongo')
+const mongo = require('./mongodb')
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', route);
 
-mongoose.connect(mongo, {useNewURLParser: true, useUnifiedTopology: true})
+mongoose.connect(mongodb, {useNewURLParser: true, useUnifiedTopology: true})
 .then(() => {
 	app.listen(3000, () => {
 	    console.log("Server has started running on 3000")
