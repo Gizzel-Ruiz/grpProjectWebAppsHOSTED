9039055b97cce2bb9b81890543b8d97327e6d5e5
Gizzel-Ruiz <gruiz015@jaguar.tamu.edu>
2022-07-31 12:15:55 -0500

    updating register/signin logic

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose');
 const route = require('./route/pages');
 const path = require('path');
 const app = express(); 
+const flash = require('connect-flash')
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', route);
+app.use(flash());
 
 mongoose.connect(mongo, {useNewURLParser: true, useUnifiedTopology: true})
 .then(() => {
