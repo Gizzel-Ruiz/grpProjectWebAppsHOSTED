634689d6756748a58cd04d60c715b90f58817559
Gizzel-Ruiz <gruiz015@jaguar.tamu.edu>
2022-08-07 13:45:42 -0500

    H test

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mongo = require('./mongo') 
+// const mongo = require('./mongo') 
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
 const mongoose = require('mongoose'); 
@@ -6,17 +6,17 @@ const route = require('./route/pages');
 const path = require('path'); 
 const app = express();  
 const session = require('express-session'); 
-const scrt = require('./scrt') 
+// const scrt = require('./scrt') 
 
-app.use(session({secret: scrt, resave:false, saveUninitialized:true})) 
+app.use(session({secret: process.env.SCRT, resave:false, saveUninitialized:true})) 
 app.set('view engine', 'ejs'); 
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.use('/', route); 
 
-mongoose.connect(mongo, {useNewURLParser: true, useUnifiedTopology: true}) 
+mongoose.connect(process.env.MONGODB_URI, {useNewURLParser: true, useUnifiedTopology: true}) 
 .then(() => { 
-    app.listen(3000, () => { 
+    app.listen(process.env.PORT || 3000, () => { 
         console.log("Server has started running on 3000") 
     }) 
 }) 
